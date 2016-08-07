f169c9fcc050bdbec07e24fefab2519a38d19d6b
robisop <Robin@Roberts-MacBook-Pro.local>
2016-08-07 11:10:46 +0200

    create_dosla_platba

--- database/migrations/2016_07_27_210826_create_dosla_platba.php
+++ database/migrations/2016_07_27_210826_create_dosla_platba.php
@@ -14,6 +14,14 @@ class CreateDoslaPlatba extends Migration
     {
         Schema::create('dosla_platba', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('iban', 50);
+            $table->string('vs', 10);
+            $table->string('nazov');
+            $table->string('poznamka');
+            $table->text('popis');
+            $table->date('datum_platby');
+            $table->date('datum_spracovania');
+            $table->unsignedTinyInteger('id_dosla_platba_stav')->nullable();
             $table->timestamps();
         });
     }
