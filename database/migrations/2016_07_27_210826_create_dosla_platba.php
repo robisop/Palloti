c36c0ee9073bdc5dd34edd354b9e3955ac50dd1d
robisop <Robin@Roberts-MacBook-Pro.local>
2016-08-07 11:35:16 +0200

    create priradena platba

--- database/migrations/2016_07_27_210826_create_dosla_platba.php
+++ database/migrations/2016_07_27_210826_create_dosla_platba.php
@@ -12,17 +12,26 @@ class CreateDoslaPlatba extends Migration
      */
     public function up()
     {
+        Schema::create('dosla_platba_stav', function (Blueprint $table) {
+            $table->unsignedTinyInteger('id', true);
+            $table->string('nazov');
+            $table->timestamps();
+        });
+
         Schema::create('dosla_platba', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('iban', 50);
-            $table->string('vs', 10);
-            $table->string('nazov');
-            $table->string('poznamka');
-            $table->text('popis');
-            $table->date('datum_platby');
-            $table->date('datum_spracovania');
+            $table->string('iban', 50)->nullable();
+            $table->string('vs', 10)->nullable();
+            $table->decimal('suma', 10, 2);
+            $table->string('nazov')->nullable();
+            $table->string('poznamka_pre_prijemcu')->nullable();
+            $table->text('popis')->nullable();
+            $table->date('datum_platby')->nullable();
+            $table->date('datum_spracovania')->nullable();
             $table->unsignedTinyInteger('id_dosla_platba_stav')->nullable();
             $table->timestamps();
+
+            $table->foreign('id_dosla_platba_stav')->references('id')->on('dosla_platba_stav');
         });
     }
 
